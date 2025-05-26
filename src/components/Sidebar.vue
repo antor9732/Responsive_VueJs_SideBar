55fa67392aca8eab95d7605c475b1b1fd3c2a098
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2025-05-26 14:20:51 +0600

    update sidebar file

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,58 +1,18 @@
 <script setup>
 import { ref } from "vue";
-
-const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
-
-
-
-const ToggleMenu = () => {
-  is_expanded.value = !is_expanded.value
-    localStorage.setItem("is_expanded", is_expanded.value)
-};
+import navMenu  from "./Sidebar/navMenu.vue";
 </script>
+
+
 <template>
-  <aside :class="`${is_expanded && 'is_expanded'}`">
-    <div class="logo">
-      <img src="../assets/logo.svg" alt="vue" />
-    </div>
-
-    <div class="menu-toggle-wrap">
-      <button class="menu-toggle" @click="ToggleMenu">
-        <span class="material-icons">keyboard_double_arrow_right</span>
-      </button>
-    </div>
-    <h1>Menu</h1>
-    <div class="menu">
-      <router-link class="button" to="/">
-        <span class="material-icons">home</span>
-        <span class="text">Home</span>
-      </router-link>
-      <router-link class="button" to="/about">
-        <span class="material-icons">contact_page</span>
-        <span class="text">About</span>
-      </router-link>
-      <router-link class="button" to="/team">
-        <span class="material-icons">groups</span>
-        <span class="text">Team</span>
-      </router-link>
-      <router-link class="button" to="/contact">
-        <span class="material-icons">contacts</span>
-        <span class="text">Contact</span>
-      </router-link>
-    </div>
-
-    <div class="flex"></div>
-
-    <div class="menu">
-         <router-link class="button" to="/settings">
-        <span class="material-icons">settings</span>
-        <span class="text">Settings</span>
-      </router-link>
-    </div>
-  </aside>
+   <div >
+    <navMenu />
+   </div>
 </template>
 
-<style lang="scss" scoped>
+
+
+<style lang="scss">
 aside {
   display: flex;
   flex-direction: column;
@@ -76,6 +36,7 @@ aside {
     margin-bottom: 1rem;
     img {
       width: 2rem;
+      color: var(--light)
     };
   };
   h1,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import home from "../views/home.vue";
-import about from "../views/about.vue";
+import home from "@/views/home.vue";
+import about from "@/views/about.vue";
+import contact from "@/views/contact.vue";
+import team  from "@/views/team.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: "/about",
       name: "about",
       component: about,
+   },
+   {
+    path: "/contact",
+    name: "contact",
+    component: contact,
+   },
+   {
+    path: "/team",
+    name: "team",
+    component: team,
    }
   ],
 });
